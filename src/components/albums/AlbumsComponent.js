e6951299533e80c7f9be9b8c05ca2e4abec99a5d
yurcha3777 <ivantsiv0683@gmail.com>
2022-08-17 19:42:54 +0300

    boss

--- src/components/albums/AlbumsComponent.js
+++ src/components/albums/AlbumsComponent.js
@@ -1,5 +1,13 @@
+import {useEffect, useState} from "react";
+
 export default function AlbumsComponent() {
     return (
+        const [albums,setAlbums] = useState([]);
+        useEffect(()=>{
+
+
+        },[])
+
 
         <div>
 
