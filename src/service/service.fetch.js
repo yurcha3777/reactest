309006a327d93d12b210bd64f12a228e65fd1ee8
yurcha3777 <ivantsiv0683@gmail.com>
2022-10-04 12:19:26 +0300

    boss

--- src/service/service.fetch.js
+++ src/service/service.fetch.js
@@ -1,1 +1,6 @@
-let baseUrl = ''
+let baseUrl = 'https://jsonplaceholder.typicode.com';
+
+const getPosts = () => fetch(baseUrl + '/posts').then(value => value.json())
+const getComments = () => fetch(baseUrl + '/comments').then(value => value.json())
+
+export {getPosts,getComments};
