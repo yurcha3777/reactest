205a60ebeb45c765284b955c6e9e880a39e2a9ce
yurcha3777 <ivantsiv0683@gmail.com>
2022-11-04 20:19:50 +0200

    boss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
+  let state = useSelector(state => state);
+  let dispatch = useDispatch();
   return (
     <div>
+        {state}
+      <button onClick={() =>{
+        dispatch({type:"INC",payload: 10})
+      }}>INC
+      </button>
+     <button onClick={() =>{
+        dispatch({type:"DEC",payload: 2})
+      }}>DEC
+      </button>
+
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,11 +5,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import App from './App';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
+let magnifierReducer = ((state = 0, action) => {
+    switch (action.type) {
+        case "INC":
+            return state + action.payload;
+        case "DEC":
+            return state - action.payload;
+        default:
+            return state;
+    }
+
+})
+
+let store = createStore(magnifierReducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
 );
 
