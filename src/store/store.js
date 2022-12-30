59502cd154cdf7fb869021a4cc6f9653e47ac183
yurcha3777 <ivantsiv0683@gmail.com>
2022-12-30 22:34:58 +0200

    boss

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,9 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {userSlice} from "../slice/userSlice";
+
+const rootreducer = combineReducers({
+
+})
 
 export const store = configureStore({
     reducer:{
